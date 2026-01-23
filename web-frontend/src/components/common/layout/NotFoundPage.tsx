3e5a9f4b83409df78df60550ef89c8eb723a3f3b
Tuienn <tuyenboy1234@gmail.com>
2026-01-23 13:58:34 +0700

    optimize render svg

--- web-frontend/src/components/common/layout/NotFoundPage.tsx
+++ web-frontend/src/components/common/layout/NotFoundPage.tsx
@@ -2,7 +2,7 @@ import Button from '@mui/material/Button'
 import Stack from '@mui/material/Stack'
 import Typography from '@mui/material/Typography'
 import { Link, useRouter } from '@tanstack/react-router'
-import svg404 from '../../../assets/svg/404.svg'
+import NotFoundSvg from '../../../assets/svg/404.svg' // import SVG như image asset
 import { useTranslation } from 'react-i18next'
 
 const NotFoundPage: React.FC = () => {
@@ -11,7 +11,12 @@ const NotFoundPage: React.FC = () => {
 
     return (
         <Stack minHeight='100vh' alignItems='center' justifyContent='center' gap={1.5} px={2}>
-            <img src={svg404} alt='404' width={220} />
+            {/* 
+              Dùng <img> vì SVG này là illustration tĩnh (404 page),
+              không cần đổi màu / animation / interaction.
+              Cách này nhẹ, đơn giản và tận dụng cache tốt.
+            */}
+            <img src={NotFoundSvg} alt='404 not found' width={200} loading='lazy' />
 
             <Typography variant='h4' textAlign='center' fontWeight={600}>
                 {t('notFound.title')}
@@ -25,6 +30,7 @@ const NotFoundPage: React.FC = () => {
                 <Button onClick={() => router.history.back()} variant='outlined' size='large'>
                     {t('notFound.backButton')}
                 </Button>
+
                 <Button component={Link} to='/' variant='contained' size='large'>
                     {t('notFound.homeButton')}
                 </Button>
