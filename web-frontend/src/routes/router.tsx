c6fdac8017c3d43bccf3b5151de9e65105dd1252
Tuienn <tuyenboy1234@gmail.com>
2026-01-23 22:24:25 +0700

    add tsr cli

--- web-frontend/src/routes/router.tsx
+++ web-frontend/src/routes/router.tsx
@@ -1,6 +1,6 @@
 import { createRouter, lazyRouteComponent } from '@tanstack/react-router'
-import { routeTree } from './routeTree.gen'
 import SuspendComponent from '../components/common/layout/SuspendComponent'
+import { routeTree } from '../routeTree.gen'
 
 export const router = createRouter({
     routeTree,
